c784b5ef2df040b1003ebd971d56da317aa6b447
Alessandro Lupo <55749200+LupoA@users.noreply.github.com>
2020-09-24 18:13:40 +0100

    Update corefcts.h

--- corefcts.h
+++ corefcts.h
@@ -8,12 +8,15 @@ void bg_method(mpfr_t, mpfr_t*);
 void rm_method(mpfr_t, mpfr_t, mpfr_t*);
 void rm_method_cosh(mpfr_t, mpfr_t, mpfr_t*);
 void rm_method_cosh_lvl1(mpfr_t, mpfr_t, mpfr_t*);
+void transform_only_step1(mpfr_t*, double*);
 void transform(mpfr_t, mpfr_t, mpfr_t*, mpfr_t*, double*, double*, double*);
 void transform_lvl1(mpfr_t, mpfr_t, mpfr_t*, mpfr_t*, double*, double*, double*);
 void deltabar(mpfr_t, mpfr_t);
+void deltabar_lvl1(mpfr_t, mpfr_t);
 void reconstructed_error(mpfr_t, mpfr_t);
 
 void set_params(double, double, int, double);
 void set_time_parms(int, int, int);
 
 #endif
+
